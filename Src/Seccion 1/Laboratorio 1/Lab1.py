d443ea807ef6808097a7d43f4cc7ec6fa86cf9c8
ManuelHenao2003 <manuelestebanhenaomarin@gmail.com>
2026-04-29 22:06:57 -0500

    Add files via upload

---
+++ Src/Seccion 1/Laboratorio 1/Lab1.py
@@ -0,0 +1,9 @@
+print ("¡Hola, Mundo!")
+print ("Manuel")
+print ("Sara") 
+print ("Sara")
+
+print ('Hola Mundo') #con comillas simples funciona igual 
+print ("¡Hola, Mundo!"), print ("¡Hola, Mundo!") #Al poner la funcion en una misma linea, delclara el error:
+#Las declaraciones deben ir separadas por nuevas líneas o punto y coma.
+print ("¡Hola, Mundo!"), print ("Sara") #Al separar con coma no hay errores  
